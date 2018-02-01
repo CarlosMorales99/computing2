c24ab3dc6eec3db6584121beb523a8f2f2933186
Carlos Morales <morales.carlos1999@gmail.com>
2018-02-01 10:10:39 -0400

    gets() to getline()

--- cookingBook.cpp
+++ cookingBook.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string.h>
+#include <ctype.h>
 class RECETA
 {
 	public:
@@ -9,21 +10,21 @@ class RECETA
 		void mostrarSinIngredientes();
 	private:
 		char nombre[50], origen[50];
-		char tipo; //entrada= e, principal = p, postre = d	
+		char tipo; //entrada= e, principal = p, postre = d
 		int cantIng;//Numero de ingredientes diferentes que posee la receta
 		float cantidad[20],calorias[20];//Ingredientes
 		char nombreIng[20][50];//Ingredientes
 		float caloriasTot;
-};	
+};
 void RECETA::registrar()
 {
 	caloriasTot=0;
 	std::cout<<"\n\nDar nombre de la receta: ";
 	std::cin.sync();
-	gets(nombre);
+	std::cin.getline(nombre,50);
 	std::cout<<"Dar pais de origen de la receta: ";
 	std::cin.sync();
-	gets(origen);
+	std::cin.getline(origen,50);
 	do
 	{
 		std::cout<<"Dar tipo de plato(entrada=e,principal=p,postre=d): ";
@@ -39,7 +40,7 @@ void RECETA::registrar()
 		std::cout<<"\nINGREDIENTE "<<i+1<<":\n";
 		std::cout<<"Dar nombre del ingrediente: ";
 		std::cin.sync();
-		gets(nombreIng[i]);
+		std::cin.getline(nombreIng[i],50);
 		std::cout<<"Dar cantidad del ingrediente: ";
 		std::cin>>cantidad[i];
 		std::cout<<"Dar cantidad de calorias del ingrediente: ";
@@ -55,10 +56,10 @@ void RECETA::busquedaRec(char pais[50],char plato)
 		std::cout<<"\nIngredientes: \n";
 		for(int k=0;k<cantIng;k++)
 		{
-			std::cout<<cantidad[k]<<" "<<nombreIng[k]<<", "<<calorias[k]<<" Cal\n";	
+			std::cout<<cantidad[k]<<" "<<nombreIng[k]<<", "<<calorias[k]<<" Cal\n";
 		}
 	}
-	
+
 }
 
 float RECETA::devolverCalorias()
@@ -71,7 +72,7 @@ void RECETA::mostrarSinIngredientes()
 	std::cout<<"\nPais: "<<origen;
 	std::cout<<"\nTipo de plato: "<<tipo;
 }
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
 	RECETA lista[100];
 	int nRecetas;
@@ -84,10 +85,10 @@ int main(int argc, char** argv)
 	{
 		lista[i].registrar();
 	}
-	
+
 	std::cout<<"\n\nDar pais a buscar: ";
 	std::cin.sync();
-	gets(pais);
+	std::cin.getline(pais,50);
 	do
 	{
 		std::cout<<"Dar tipo de plato a buscar(entrada=e,principal=p,postre=d): ";
@@ -100,22 +101,21 @@ int main(int argc, char** argv)
 	for(int i=0;i<nRecetas;i++)
 	{
 		lista[i].busquedaRec(pais,tipoPlato);
-	}	
-	
+	}
+
 	for(int i=0;i<nRecetas;i++)
 	{
 		if(i==0||lista[i].devolverCalorias()>mayCantCalorias)
 		{
 			mayCantCalorias=lista[i].devolverCalorias();
-			recMayCalorias=i;	
-		}	
+			recMayCalorias=i;
+		}
 	}
 	std::cout<<"\n\nEl tipo de plato con mayor valor calorico es: \n";
 	lista[recMayCalorias].mostrarSinIngredientes();
-	
+
 	std::cout<<"\n\nPresione una tecla para continuar . . .";
 	std::cin.sync();
 	std::cin.get();
 	return 0;
 }
-
